e4d9e2199e9e2f285065be44b81245d2758625d5
Soumy Gupta <soumyg76@gmail.com>
2023-03-09 22:52:54 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,9 +3,9 @@ public class Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int a = 2 , b =4;
-		System.out.println(a+b);
+		System.out.println("Sum is "+a+b);
 	}
 
 }
